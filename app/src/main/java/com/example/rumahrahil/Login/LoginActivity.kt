2352f20c5a6279e444c135f160c309fa0499c4f4
alamsyaah <alamsyaah1198@gmail.com>
2020-11-10 21:36:33 +0700

    update bottom nav dan fragment

--- app/src/main/java/com/example/rumahrahil/Login/LoginActivity.kt
+++ app/src/main/java/com/example/rumahrahil/Login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.rumahrahil.Login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rumahrahil.MainActivity
 import com.example.rumahrahil.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -12,6 +13,10 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         tv_gotosignup_login.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        btn_submit_login.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
