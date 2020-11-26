09fe3101725af43b54f5c5c36273f4a2fe083034
Alif Andarta <alifandartaa@yahoo.com>
2020-11-26 15:51:12 +0700

    add layout video pembelajaran

---
+++ app/src/main/java/com/example/rumahrahil/video/ListVideoAdapter.kt
@@ -0,0 +1,71 @@
+package com.example.rumahrahil.video
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.rumahrahil.R
+import com.example.rumahrahil.video.model.Video
+import kotlinx.android.synthetic.main.item_vidcategory.view.*
+
+class ListVideoAdapter() :
+        RecyclerView.Adapter<ListVideoAdapter.MyViewHolder>() {
+
+    var listVideo = arrayListOf<Video>()
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(video: Video) {
+            with(itemView) {
+                Glide.with(this)
+                        .load(video.linkThumbnail)
+                        .override(300, 150)
+                        .into(iv_thumbnail_video)
+            }
+        }
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    fun addToListAdapter(list: ArrayList<Video>) {
+        listVideo.clear()
+        listVideo.addAll(list)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): ListVideoAdapter.MyViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_vidcategory, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+        return MyViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.bind(listVideo[position])
+//        holder.itemView.setOnClickListener {
+//            onItemClickCallback.onItemClick(listVideo[holder.adapterPosition])
+//        }
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClick(listVideo[holder.adapterPosition])
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = listVideo.size
+
+    interface OnItemClickCallback {
+        fun onItemClick(data: Video)
+    }
+}
---
+++ app/src/main/java/com/example/rumahrahil/video/model/Video.kt
@@ -0,0 +1,12 @@
+package com.example.rumahrahil.video.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Video(
+        var nama: String? = null,
+        var linkVideo: String? = null,
+        var linkThumbnail: String? = null
+) : Parcelable
+
