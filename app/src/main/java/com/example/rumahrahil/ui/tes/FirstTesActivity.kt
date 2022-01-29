b94945c0c0a28c9298167b6d4ca63627528a5905
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2022-01-29 17:00:08 +0700

    fix logic tes activity

--- app/src/main/java/com/example/rumahrahil/ui/tes/FirstTesActivity.kt
+++ app/src/main/java/com/example/rumahrahil/ui/tes/FirstTesActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rumahrahil.ui.tes
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
@@ -8,9 +9,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.rumahrahil.R
 import com.example.rumahrahil.databinding.ActivityFirstTesBinding
+import com.example.rumahrahil.ui.MainActivity
 import com.example.rumahrahil.utils.Constants
 import com.example.rumahrahil.utils.MySharedPreferences
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
 import es.dmoral.toasty.Toasty
 
 
@@ -20,7 +24,6 @@ class FirstTesActivity : AppCompatActivity() {
     private lateinit var questionViewModel: QuestionViewModel
     private lateinit var studentId: String
     private lateinit var mySharedPreferences: MySharedPreferences
-    private lateinit var mQuiz: ArrayList<QuestionEntity>
     private var soal = ArrayList<String>()
     private var optionA = ArrayList<String>()
     private var optionB = ArrayList<String>()
@@ -28,14 +31,12 @@ class FirstTesActivity : AppCompatActivity() {
     private var optionD = ArrayList<String>()
     private var optionE = ArrayList<String>()
     private var correctAnswer = ArrayList<String>()
-    private var cthPaketId: String = "201"
-    private var cthMapelId: String = "79"
     var mCurrentPosition: Int = 1
     var answerIndex: Int = 0
     var mScore: Int = 0
+    var timeQuiz: String = "120"
     lateinit var countdown_timer: CountDownTimer
     var time_in_milli_seconds = 0L
-    val timeQuiz: String = "120"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,22 +47,27 @@ class FirstTesActivity : AppCompatActivity() {
 
         mySharedPreferences = MySharedPreferences(this)
         studentId = mySharedPreferences.getValue(Constants.SISWA_ID)!!
+        val idPaket = intent.getStringExtra(Constants.PAKET_ID)!!
         val tokenAuth = mySharedPreferences.getValue(Constants.TOKEN).toString()
+
+
+        Log.d("tes", "id Paket : $idPaket")
+        Log.d("tes", "id token : $tokenAuth")
 
         questionViewModel = ViewModelProvider(
             this,
             ViewModelProvider.NewInstanceFactory()
         ).get(QuestionViewModel::class.java)
 
-        mQuiz = arrayListOf<QuestionEntity>()
 
-        getQuestion(cthMapelId, cthPaketId, tokenAuth)
+        getQuestion(idPaket, tokenAuth)
+
 
         mFirstTesBinding.btnNext.setOnClickListener {
             if (mFirstTesBinding.rbFirstAnswer.isChecked || mFirstTesBinding.rbSecondAnswer.isChecked || mFirstTesBinding.rbThirdAnswer.isChecked || mFirstTesBinding.rbFourthAnswer.isChecked || mFirstTesBinding.rbFifthAnswer.isChecked) {
                 mCurrentPosition++
-//                answerIndex+1
-                getQuestion(cthMapelId, cthPaketId, tokenAuth)
+//                answerIndex++
+                getQuestion(idPaket, tokenAuth)
             } else {
                 Toasty.error(this, "Jawaban Belum Anda Belum Pilih", Toast.LENGTH_SHORT).show()
             }
@@ -70,7 +76,28 @@ class FirstTesActivity : AppCompatActivity() {
         mFirstTesBinding.btnPrevious.setOnClickListener {
             mCurrentPosition--
 //            answerIndex--
-            getQuestion(cthPaketId, cthMapelId, tokenAuth)
+            getQuestion(idPaket, tokenAuth)
+        }
+
+        mFirstTesBinding.btnFinish.setOnClickListener {
+            val mDialog = MaterialDialog.Builder(this)
+                .setTitle(getString(R.string.selesai_ujian))
+                .setMessage(getString(R.string.message_dialog_finish))
+                .setCancelable(true)
+                .setPositiveButton(
+                    getString(R.string.no),
+                    R.drawable.dialog_close
+                ) { dialogInterface, which ->
+                    dialogInterface.dismiss()
+                }
+                .setNegativeButton(
+                    getString(R.string.yes), R.drawable.ic_check
+                ) { dialogInterface, _ ->
+                    startActivity(Intent(this, MainActivity::class.java))
+                    dialogInterface.dismiss()
+                }
+                .build()
+            mDialog.show()
         }
 
         time_in_milli_seconds = timeQuiz.toLong() * 60000L
@@ -78,11 +105,20 @@ class FirstTesActivity : AppCompatActivity() {
 
     }
 
-    private fun getQuestion(cthMapelId: String, cthPaketId: String, tokenAuth: String) {
-        questionViewModel.getQuestion(cthMapelId, cthPaketId, tokenAuth)
-        questionViewModel.mQuestion.observe(this, { mQuestion ->
+    private fun getQuestion(idPaket: String, tokenAuth: String) {
+        questionViewModel.getQuestion(idPaket, tokenAuth)
+        questionViewModel.mQuestion.observe(this) { mQuestion ->
 
             if (mQuestion != null) {
+
+                soal.clear()
+                optionA.clear()
+                optionB.clear()
+                optionC.clear()
+                optionD.clear()
+                optionE.clear()
+                correctAnswer.clear()
+
                 mQuestion.forEach {
                     soal.add(it.soal)
                     optionA.add(it.option_a)
@@ -92,98 +128,156 @@ class FirstTesActivity : AppCompatActivity() {
                     optionE.add(it.option_e)
                     correctAnswer.add(it.jawaban_benar)
                 }
-
-                mFirstTesBinding.btnPrevious.visibility = View.GONE
-
-                if ((mCurrentPosition - 1) == answerIndex) {
-                    mFirstTesBinding.btnPrevious.visibility = View.INVISIBLE
-                } else {
-                    mFirstTesBinding.btnPrevious.visibility = View.VISIBLE
-                }
-
-//                val maxNumber = soal.size
-                val question = soal[mCurrentPosition - 1]
-                val firstOption = optionA[mCurrentPosition - 1]
-                val secondOption = optionB[mCurrentPosition - 1]
-                val thirdOption = optionC[mCurrentPosition - 1]
-                val fourthOption = optionD[mCurrentPosition - 1]
-                val fifthOption = optionE[mCurrentPosition - 1]
-                val realOption = correctAnswer[answerIndex]
-
-                if (mFirstTesBinding.rbFirstAnswer.isChecked) {
-
-                    Log.d("KunciJawaban = ", realOption)
-                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFirstAnswer.text as String)
-
-                    if (mFirstTesBinding.rbFirstAnswer.text != realOption) {
-                        Log.d("Gagal", "Salah")
-                    } else {
-                        Log.d("Sukses", "benar")
-//                        mySharedPreferences.saveArrayList(Constants.optionA, mFirstTesBinding.rbFirstAnswer.text)
-//                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFirstAnswer.text)
-                    }
-                } else if (mFirstTesBinding.rbSecondAnswer.isChecked) {
-
-                    Log.d("KunciJawaban = ", realOption)
-                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbSecondAnswer.text as String)
-
-                    if (mFirstTesBinding.rbSecondAnswer.text != realOption) {
-                        Log.d("Gagal", "Salah")
-                    } else {
-                        Log.d("Sukses", "benar")
-//                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbSecondAnswer.text)
-                    }
-                } else if (mFirstTesBinding.rbThirdAnswer.isChecked) {
-
-                    Log.d("KunciJawaban = ", realOption)
-                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbThirdAnswer.text as String)
-
-                    if (mFirstTesBinding.rbThirdAnswer.text != realOption) {
-                        Log.d("Gagal", "Salah")
-                    } else {
-                        Log.d("Sukses", "benar")
-//                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbThirdAnswer.text)
-                    }
-                } else if (mFirstTesBinding.rbFourthAnswer.isChecked) {
-
-                    Log.d("KunciJawaban = ", realOption)
-                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFourthAnswer.text as String)
-
-                    if (mFirstTesBinding.rbFourthAnswer.text != realOption) {
-                        Log.d("Gagal", "Salah")
-                    } else {
-                        Log.d("Sukses", "benar")
-//                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFourthAnswer.text)
-                    }
-                } else if (mFirstTesBinding.rbFifthAnswer.isChecked) {
-
-                    Log.d("KunciJawaban = ", realOption)
-                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFifthAnswer.text as String)
-
-                    if (mFirstTesBinding.rbFifthAnswer.text != realOption) {
-                        Log.d("Gagal", "Salah")
-                    } else {
-                        Log.d("Sukses", "benar")
-//                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFifthAnswer.text)
-                    }
-                } else {
-                    Log.d("ERROR", "Tidak Ada Jawaban yang dipilih")
-                }
-
-                with(mFirstTesBinding) {
-                    tvFirstNumber.text = "$mCurrentPosition"
-//                    tvLastNumber.text = maxNumber.toString()
-                    tvQuestion.text = question
-                    rbFirstAnswer.text = firstOption
-                    rbSecondAnswer.text = secondOption
-                    rbThirdAnswer.text = thirdOption
-                    rbFourthAnswer.text = fourthOption
-                    rbFifthAnswer.text = fifthOption
-//                    radioGroupAnswer.clearCheck()
-                }
             }
-        })
+
+            mFirstTesBinding.btnPrevious.visibility = View.GONE
+            mFirstTesBinding.btnNext.visibility = View.GONE
+            mFirstTesBinding.btnFinish.visibility = View.GONE
+
+            if ((mCurrentPosition - 1) == answerIndex) {
+                mFirstTesBinding.btnPrevious.visibility = View.INVISIBLE
+                mFirstTesBinding.btnNext.visibility = View.VISIBLE
+            } else if (mCurrentPosition == soal.size) {
+                mFirstTesBinding.btnPrevious.visibility = View.VISIBLE
+                mFirstTesBinding.btnFinish.visibility = View.VISIBLE
+            } else {
+                mFirstTesBinding.btnPrevious.visibility = View.VISIBLE
+                mFirstTesBinding.btnNext.visibility = View.VISIBLE
+            }
+
+
+            val maxNumber = soal.size
+            val question = soal[mCurrentPosition - 1]
+            val firstOption = optionA[mCurrentPosition - 1]
+            val secondOption = optionB[mCurrentPosition - 1]
+            val thirdOption = optionC[mCurrentPosition - 1]
+            val fourthOption = optionD[mCurrentPosition - 1]
+            val fifthOption = optionD[mCurrentPosition - 1]
+
+            with(mFirstTesBinding) {
+                rbFirstAnswer.text = "a"
+                rbSecondAnswer.text = "b"
+                rbThirdAnswer.text = "c"
+                rbFourthAnswer.text = "d"
+                rbFifthAnswer.text = "e"
+                tvQuestion.text = question
+                tvFirstAnswer.text = firstOption
+                tvSecondAnswer.text = secondOption
+                tvThirdAnswer.text = thirdOption
+                tvFourthAnswer.text = fourthOption
+                tvFifthAnswer.text = fifthOption
+                tvLastNumber.text = maxNumber.toString()
+                tvFirstNumber.text = mCurrentPosition.toString()
+            }
+        }
     }
+
+//    private fun getQuestion(idPaket: String, tokenAuth: String) {
+//        questionViewModel.getQuestion(idPaket, tokenAuth)
+//        questionViewModel.mQuestion.observe(this, { mQuestion ->
+//
+//            if (mQuestion != null) {
+//                mQuestion.forEach {
+//                    soal.add(it.soal)
+//                    optionA.add(it.option_a)
+//                    optionB.add(it.option_b)
+//                    optionC.add(it.option_c)
+//                    optionD.add(it.option_d)
+//                    optionE.add(it.option_e)
+//                    correctAnswer.add(it.jawaban_benar)
+//                }
+//
+//                mFirstTesBinding.btnPrevious.visibility = View.GONE
+//
+//                if ((mCurrentPosition - 1) == answerIndex) {
+//                    mFirstTesBinding.btnPrevious.visibility = View.INVISIBLE
+//                } else {
+//                    mFirstTesBinding.btnPrevious.visibility = View.VISIBLE
+//                }
+//
+////                val maxNumber = soal.size
+//                val question = soal[mCurrentPosition - 1]
+//                val firstOption = optionA[mCurrentPosition - 1]
+//                val secondOption = optionB[mCurrentPosition - 1]
+//                val thirdOption = optionC[mCurrentPosition - 1]
+//                val fourthOption = optionD[mCurrentPosition - 1]
+//                val fifthOption = optionE[mCurrentPosition - 1]
+//                val realOption = correctAnswer[answerIndex]
+
+//                if (mFirstTesBinding.rbFirstAnswer.isChecked) {
+//
+//                    Log.d("KunciJawaban = ", realOption)
+//                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFirstAnswer.text as String)
+//
+//                    if (mFirstTesBinding.rbFirstAnswer.text != realOption) {
+//                        Log.d("Gagal", "Salah")
+//                    } else {
+//                        Log.d("Sukses", "benar")
+////                        mySharedPreferences.saveArrayList(Constants.optionA, mFirstTesBinding.rbFirstAnswer.text)
+////                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFirstAnswer.text)
+//                    }
+//                } else if (mFirstTesBinding.rbSecondAnswer.isChecked) {
+//
+//                    Log.d("KunciJawaban = ", realOption)
+//                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbSecondAnswer.text as String)
+//
+//                    if (mFirstTesBinding.rbSecondAnswer.text != realOption) {
+//                        Log.d("Gagal", "Salah")
+//                    } else {
+//                        Log.d("Sukses", "benar")
+////                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbSecondAnswer.text)
+//                    }
+//                } else if (mFirstTesBinding.rbThirdAnswer.isChecked) {
+//
+//                    Log.d("KunciJawaban = ", realOption)
+//                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbThirdAnswer.text as String)
+//
+//                    if (mFirstTesBinding.rbThirdAnswer.text != realOption) {
+//                        Log.d("Gagal", "Salah")
+//                    } else {
+//                        Log.d("Sukses", "benar")
+////                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbThirdAnswer.text)
+//                    }
+//                } else if (mFirstTesBinding.rbFourthAnswer.isChecked) {
+//
+//                    Log.d("KunciJawaban = ", realOption)
+//                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFourthAnswer.text as String)
+//
+//                    if (mFirstTesBinding.rbFourthAnswer.text != realOption) {
+//                        Log.d("Gagal", "Salah")
+//                    } else {
+//                        Log.d("Sukses", "benar")
+////                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFourthAnswer.text)
+//                    }
+//                } else if (mFirstTesBinding.rbFifthAnswer.isChecked) {
+//
+//                    Log.d("KunciJawaban = ", realOption)
+//                    Log.d("Jawaban Anda = ", mFirstTesBinding.rbFifthAnswer.text as String)
+//
+//                    if (mFirstTesBinding.rbFifthAnswer.text != realOption) {
+//                        Log.d("Gagal", "Salah")
+//                    } else {
+//                        Log.d("Sukses", "benar")
+////                        mySharedPreferences.setValue(Constants.optionA, mFirstTesBinding.rbFifthAnswer.text)
+//                    }
+//                } else {
+//                    Log.d("ERROR", "Tidak Ada Jawaban yang dipilih")
+//                }
+
+//                with(mFirstTesBinding) {
+//                    tvFirstNumber.text = "$mCurrentPosition"
+//
+//                    tvQuestion.text = question
+//                    rbFirstAnswer.text = firstOption
+//                    rbSecondAnswer.text = secondOption
+//                    rbThirdAnswer.text = thirdOption
+//                    rbFourthAnswer.text = fourthOption
+//                    rbFifthAnswer.text = fifthOption
+////                    radioGroupAnswer.clearCheck()
+//                }
+//            }
+//        })
+//    }
 
     private fun startTimer(time_in_second: Long) {
         countdown_timer = object : CountDownTimer(time_in_second, 1000) {
